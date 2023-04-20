7d890f94be79c89e54c949a39d4006e6a00f5e25
rsantacroce <rob@santacroce.xyz>
2023-04-20 13:11:49 +0200

    added a better consensus stub

--- src/consensus/consensus.lisp
+++ src/consensus/consensus.lisp
@@ -7,14 +7,39 @@
            #:process-transaction))
 (in-package :bitcoin-lisp-consensus)
 
-(defun process-block (block chain-params blockchain)
+(defun find-previous-block (block blockchain)
+  ;; TODO: Implement the function to find the previous block in the blockchain.
+  )
+
+(defun block-height (block)
+  ;; TODO: Implement the function to return the height of a block.
+  )
+
+(defun update-block-height (block height)
+  ;; TODO: Implement the function to update the height of a block.
+  )
+
+(defun remove-from-mempool (mempool tx)
+  ;; TODO: Implement the function to remove a transaction from the mempool.
+  )
+
+(defun update-best-block (blockchain block)
+  ;; TODO: Implement the function to update the best block in the blockchain.
+  )
+
+(defun process-block (block chain-params blockchain mempool)
   "Process a block and update the blockchain state."
   (unless (validate-block block chain-params)
     (error "Invalid block"))
   (let ((prev-block (find-previous-block block blockchain)))
     (unless prev-block
       (error "Previous block not found in the blockchain"))
-    (update-blockchain-state blockchain block)))
+    (let ((height (1+ (block-height prev-block))))
+      (update-block-height block height)
+      (dolist (tx (block-transactions block))
+        (remove-from-mempool mempool tx))
+      (update-blockchain-state blockchain block)
+      (update-best-block blockchain block))))
 
 (defun process-transaction (transaction chain-params mempool blockchain)
   "Process a transaction and update the mempool and blockchain state."
